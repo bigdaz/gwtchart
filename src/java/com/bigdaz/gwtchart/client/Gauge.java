db6eac0cc80891ededa38d0d04ae5a4259f4c0ec
daz <daz@greensync.com.au>
2010-02-07 19:54:10 -0700

    Gauge improvements - Declare gauge arc algorithmically, so it's easier to tweak - Fix SVG window size

--- src/java/com/bigdaz/gwtchart/client/Gauge.java
+++ src/java/com/bigdaz/gwtchart/client/Gauge.java
@@ -8,45 +8,69 @@ package com.bigdaz.gwtchart.client;
 
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONString;
+import com.hydro4ge.raphaelgwt.client.PathBuilder;
 import com.hydro4ge.raphaelgwt.client.Raphael;
 
 public class Gauge extends Raphael {
     private Path gauge;
     private Path needle;
     private double scaleFactor;
+    private static final int R = 100;
+    private static final int OFFSET = 20;
 
     public Gauge(int size, double initialValue) {
-        super(size, size);
+        super(size, (int) (size * 0.7));
 
         scaleFactor = size / 200.0;
 
-        String gaugePath = "M 100,10 a 100,100 0 0,1 86.6,50 l -43.4,25 a 50,50 0 0,0 -86.6,0 l -43.4,-25 a 100,100 0 0,1 86.6,-50 z";
+        double angle = Math.PI / 6;
+        int r = 60;
+        double cosX = Math.cos(angle);
+        double sinX = Math.sin(angle);
+
+        PathBuilder gaugePath = new PathBuilder()
+                .M(100, OFFSET)
+                .a(R, R, 0, 0, 1, R * cosX, R * sinX)
+                .l(-cosX * (R -r), sinX * (R - r))
+                .a(r, r, 0, 0, 0, -2 * r * cosX, 0)
+                .l(-cosX * (R - r), -sinX * (R -r))
+                .a(R, R, 0, 0, 1, R * cosX, -R * sinX);
+
         gauge = new Path(gaugePath);
         gauge.attr("stroke", "none");
         gauge.attr("fill", "0-green-yellow:50-red");
         gauge.scale(scaleFactor, scaleFactor, 0, 0);
 
-        String needlePath = "M 100,0 l 5,15 0,80 a 5,5 0 1,1 -10,0 l 0,-80 5,-15 z";
+        PathBuilder needlePath = new PathBuilder()
+                .M(100, 0)
+                .l(5, 15)
+                .l(0, R + OFFSET - 20)
+                .a(5, 5, 0, 1, 1, -10, 0)
+                .l(0, -R + OFFSET - 20)
+                .l(5, -15);
+
         needle = new Path(needlePath);
         needle.attr("stroke", "none");
-        needle.attr("fill", "0-#000000-#AAAAAA:35-#000000");
+        needle.attr("fill", "0-#552200-#AA9977:35-#552200");
         needle.scale(scaleFactor, scaleFactor, 0, 0);
 
         double rotation = (initialValue * 120) - 60;
-        double needlePos = 100 * scaleFactor;
+        double needlePosY = (R + 20) * scaleFactor;
+        double needlePosX = R * scaleFactor;
 
-        needle.rotate(rotation, needlePos, needlePos);
+        needle.rotate(rotation, needlePosX, needlePosY);
     }
 
     public void setValue(double initialValue) {
         double rotation = (initialValue * 120) - 60;
-        double needlePos = 100 * scaleFactor;
+        double needlePosY = (R + OFFSET) * scaleFactor;
+        double needlePosX = R * scaleFactor;
 
-        needle.rotate(rotation, needlePos, needlePos, false);
+//        needle.rotate(rotation, needlePosX, needlePosY, false);
 
         // An unable to animate the rotation relative to previous position. Grrr.
         JSONObject animation = new JSONObject();
-        animation.put("rotation", new JSONString(rotation + "," + needlePos + "," + needlePos + ", false"));
+        animation.put("rotation", new JSONString(rotation + "," + needlePosX + "," + needlePosY + ", false"));
         needle.animate(animation, 500);
     }
 
