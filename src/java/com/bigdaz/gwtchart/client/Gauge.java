4cba7b6d47159016d2fe13a555a28619301c9c3c
daz <daz@greensync.com.au>
2010-02-16 10:44:59 -0700

    Removed animation, since it was broken and kind of ugly

--- src/java/com/bigdaz/gwtchart/client/Gauge.java
+++ src/java/com/bigdaz/gwtchart/client/Gauge.java
@@ -6,8 +6,6 @@
  */
 package com.bigdaz.gwtchart.client;
 
-import com.google.gwt.json.client.JSONObject;
-import com.google.gwt.json.client.JSONString;
 import com.hydro4ge.raphaelgwt.client.PathBuilder;
 import com.hydro4ge.raphaelgwt.client.Raphael;
 
@@ -17,6 +15,7 @@ public class Gauge extends Raphael {
     private double scaleFactor;
     private static final int R = 100;
     private static final int OFFSET = 20;
+    private double currentValue;
 
     public Gauge(int size, double initialValue) {
         super(size, (int) (size * 0.7));
@@ -54,11 +53,7 @@ public class Gauge extends Raphael {
         needle.attr("fill", "0-#666666-#EEEEEE:35-#666666");
         needle.scale(scaleFactor, scaleFactor, 0, 0);
 
-        double rotation = (initialValue * 120) - 60;
-        double needlePosY = (R + 20) * scaleFactor;
-        double needlePosX = R * scaleFactor;
-
-        needle.rotate(rotation, needlePosX, needlePosY);
+        setValue(initialValue);
     }
 
     public void setValue(double initialValue) {
@@ -66,12 +61,7 @@ public class Gauge extends Raphael {
         double needlePosY = (R + OFFSET) * scaleFactor;
         double needlePosX = R * scaleFactor;
 
-//        needle.rotate(rotation, needlePosX, needlePosY, false);
-
-        // An unable to animate the rotation relative to previous position. Grrr.
-        JSONObject animation = new JSONObject();
-        animation.put("rotation", new JSONString(rotation + "," + needlePosX + "," + needlePosY + ", false"));
-        needle.animate(animation, 500);
+        needle.rotate(rotation, needlePosX, needlePosY, true);
     }
 
 }
